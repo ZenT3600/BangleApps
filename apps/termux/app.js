03c55687a524b2305841e60ceb9ccb8911c5e281
ZenT3600 <matteo.leggio@tiscali.it>
2022-09-19 06:53:04 +0200

    Begin implementing Termux

---
+++ apps/termux/app.js
@@ -0,0 +1,31 @@
+var uart; // require("ble_uart")
+var device; // BluetoothDevice
+var customCommand = "";
+// Set up terminal
+Bangle.loadWidgets();
+var R = Bangle.appRect;
+var termg = Graphics.createArrayBuffer(R.w, R.h, 1, {msb:true});
+var termVisible = false;
+termg.setFont("6x8");
+term = require("VT100").connect(termg, {
+  charWidth : 6,
+  charHeight : 8
+});
+term.print = str => {
+  for (var i of str) { term.char(i); }
+  if (termVisible) g.reset().drawImage(termg,R.x,R.y).setFont("6x8").setFontAlign(0,-1,1).drawString("MORE",R.w-1,(R.y+R.y2)/2);
+};
+
+
+function showTerminal() {
+  E.showMenu();
+  Bangle.setUI({
+    mode : "custom",
+  });
+  termVisible = true;
+  term.print(""); // redraw terminal
+}
+
+// now start
+Bangle.drawWidgets();
+showTerminal
