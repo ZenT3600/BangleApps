b26ef4e9d601559d4e4085b14cfb9adee4640c9b
ZenT3600 <matteo.leggio@tiscali.it>
2022-09-20 12:01:09 +0200

    Fixed word wrapping

--- apps/termux/app.js
+++ apps/termux/app.js
@@ -4,21 +4,36 @@ var customCommand = "";
 Bangle.loadWidgets();
 var R = Bangle.appRect;
 var termg = Graphics.createArrayBuffer(R.w, R.h, 1, {msb:true});
-var termVisible = false;
 termg.setFont("6x8");
 term = require("VT100").connect(termg, {
   charWidth : 6,
   charHeight : 8
 });
+
+/*
+const chunkString = (str, n, acc) => {
+    if (str.length === 0) {
+        return acc;
+    } else {
+        acc.push(str.substring(0, n));
+        return chunkStr(str.substring(n), n, acc);
+    }
+};
+*/
+
 term.print = str => {
-  let index = 0;
+  let index = 1;
+  /*if (str.length > 26) {
+    let cs = chunkString(str, 26, []);
+    for (var c of cs) {
+      term.print(c + "\n");
+    }
+  }*/
   for (var i of str) {
     term.char(i);
-    if (index > 26) {
-      term.char('\n');
-    }
-    index++
   }
+  //term.y += ~~(index / 26);
+  //term.x = 0;
   if (termVisible) g.reset().drawImage(termg,R.x,R.y).setFont("6x8").setFontAlign(0,-1,1).drawString("MORE",R.w-1,(R.y+R.y2)/2);
 };
 
@@ -34,4 +49,7 @@ function showTerminal() {
 
 // now start
 Bangle.drawWidgets();
-showTerminal
+showTerminal();
+
+
+
